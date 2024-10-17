18889911b7a433194f6e6c3f852213f3581503a8
M0NS13UR-K <krishnach073@gmail.com>
2024-10-17 11:06:37 -0700

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The app allows users to add, edit, and view events, focusing on building a real-world application that manages user data effectively and handles exceptions.
+In Progress.
--- obj/Debug/net8.0/CSEventSchedularApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSEventSchedularApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSEventSchedularApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a628ed7ede2220df81683ba3daaf60a78a47f233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5178843a2eafd67cd329e0abb0cbfb9edbd85fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSEventSchedularApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSEventSchedularApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
